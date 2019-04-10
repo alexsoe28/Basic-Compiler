42dd70f1eb8622458c5dc3a0eaa380a7a9b64a10
Alexander Bradley Soe <asoe@unix1.lt.ucsc.edu>
2019-04-09 18:54:55 -0700

    Added getopt stuff

--- asoe/asg1/code/string_set/main.cpp
+++ asoe/asg1/code/string_set/main.cpp
@@ -1,16 +1,26 @@
 // $Id: main.cpp,v 1.2 2016-08-18 15:13:48-07 - - $
-
+#include <unistd.h>
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
-#include <unistd.h>
+
+#include <libgen.h>
+#include <errno.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "string_set.h"
 #include "auxlib.h"
+
+string CPP = "/usr/bin/cpp -nostdinc";
+constexpr size_t LINESIZE = 1024;
+int yydebug = 0;
+int yy_flex_debug = 0;
 
 void chomp (char* string, char delim) {
    size_t len = strlen (string);
@@ -50,15 +60,43 @@ void cpplines (FILE* pipe, const char* filename) {
 }
 
 int main (int argc, char** argv) {
+   const char* execname = basename (argv[0]);
+   int exit_status = EXIT_SUCCESS;
 
-	while ((c = getopt (argc, argv, "@Dly:")) != -1)
-		switch (c)
-	for (int i = 1; i < argc; ++i) {
-    	const string* str = string_set::intern (argv[i]);
-    	printf ("intern (\"%s\") returned %p->\"%s\"\n",
-            argv[i], str, str->c_str());
-	}
-	string_set::dump (stdout);
-	return EXIT_SUCCESS;
+   int flagOption;
+   while((flagOption = getopt(argc, argv, "@Dly:")) != -1){
+      switch(flagOption){
+         case '@': set_debugflags(optarg); break; //Help
+         case 'D': CPP = CPP + "-D" + optarg; break; //Help
+         case 'l': yy_flex_debug = 1; break; 
+         case 'y': yydebug = 1; break;
+         default: perror("Usage: oc [-ly] [-@ flag ...] [-D string] program.oc\n"); break;
+      }
+   }
+   if(optind > argc){
+      perror("Usage: oc [-ly] [-@ flag ...] [-D string] program.oc\n");
+      exit(exit_status);
+   }
+
+
+   for (int argi = 1; argi < argc; ++argi) {
+      char* filename = argv[argi];
+      string command = CPP + " " + filename;
+      printf ("command=\"%s\"\n", command.c_str());
+      
+      FILE* pipe = popen (command.c_str(), "r");
+      if (pipe == nullptr){
+         exit_status = EXIT_FAILURE;
+         fprintf (stderr, "%s: %s: %s\n",
+                  execname, command.c_str(), strerror (errno));
+      }
+      else{
+         cpplines (pipe, filename);
+         int pclose_rc = pclose (pipe);
+         eprint_status (command.c_str(), pclose_rc);
+         if (pclose_rc != 0) exit_status = EXIT_FAILURE;
+      }
+   }
+   return exit_status;
 }
 
